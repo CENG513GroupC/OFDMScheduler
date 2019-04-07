c8ca30e5b02069cf30565cfe3ec45547676d6209
SaimSUNEL <nefron_kurt@hotmail.com>
2019-04-07 23:13:15 +0300

    OFDM Scheduler Draft

---
+++ build/CMakeFiles/try.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/saim/bladeRF/OFDMScheduling/src/config.c" "/home/saim/bladeRF/OFDMScheduling/build/CMakeFiles/try.dir/src/config.c.o"
+  "/home/saim/bladeRF/OFDMScheduling/src/transceive.c" "/home/saim/bladeRF/OFDMScheduling/build/CMakeFiles/try.dir/src/transceive.c.o"
+  "/home/saim/bladeRF/OFDMScheduling/src/tryframing.c" "/home/saim/bladeRF/OFDMScheduling/build/CMakeFiles/try.dir/src/tryframing.c.o"
+  "/home/saim/bladeRF/OFDMScheduling/src/utils.c" "/home/saim/bladeRF/OFDMScheduling/build/CMakeFiles/try.dir/src/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../includes"
+  "/usr/local/include"
+  "/opt/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
